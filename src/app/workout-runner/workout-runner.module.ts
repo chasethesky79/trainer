9a5dc8cbcc6c34afc3656a07e717a297a67920d1
Bharath Seshadri <bharathseshadri@Bharaths-MacBook-Pro.local>
2020-11-08 13:30:43 -0600

    master
    Reorganized imports in the workout runner component

--- src/app/workout-runner/workout-runner.module.ts
+++ src/app/workout-runner/workout-runner.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
 import { WorkoutRunnerRoutingModule } from './workout-runner-routing.module';
 import { WorkoutRunnerComponent } from './workout-runner.component';
 import { WorkoutService } from '../service/workout.service';
